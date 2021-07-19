2678ccda50440d1cacb4bf74bf93946edbc52827
植村勇太 <uemurayuuta@uemurayuutanoMacBook-Pro.local>
2021-07-20 07:45:27 +0900

    create models

---
+++ backend/seeders/20210719122347-sample-user.js
@@ -0,0 +1,39 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    const now = new Date();
+    return await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          name: "yuta",
+          pass: "password",
+          createdAt: now,
+          updatedAt: now,
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    return queryInterface.bulkDelete("Users", null, {});
+  },
+};
