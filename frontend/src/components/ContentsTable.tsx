3acf91696d0c222b12cd88be8b81e970cb0575f8
植村勇太 <uemurayuuta@uemurayuutanoMacBook-Pro.local>
2021-07-25 14:17:40 +0900

    add pagination by using react-paginate

--- frontend/src/components/ContentsTable.tsx
+++ frontend/src/components/ContentsTable.tsx
@@ -1,4 +1,4 @@
-import React, { VFC } from "react";
+import React, { VFC, useState } from "react";
 import {
   withStyles,
   Theme,
@@ -12,6 +12,7 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import ReactPaginate from "react-paginate";
 
 const StyledTableCell = withStyles((theme: Theme) =>
   createStyles({
@@ -61,38 +62,65 @@ const cutDescription = (description: string): string => {
 };
 
 const ContentsTable: VFC<{ contents: Content[] }> = ({ contents }) => {
+  const [offset, setOffset] = useState(0);
+  const perPage: number = 10;
+
   const classes = useStyles();
-  console.log(contents[0]);
+
+  const handlePageChange = (data: { selected: number }) => {
+    let page_number = data["selected"];
+    setOffset(page_number * perPage);
+  };
   return (
-    <TableContainer component={Paper} className={classes.root}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell align="center">タイトル</StyledTableCell>
-            <StyledTableCell align="center">カテゴリー</StyledTableCell>
-            <StyledTableCell align="center">URL</StyledTableCell>
-            <StyledTableCell align="center">説明</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {contents.map((content) => (
-            <StyledTableRow>
-              <StyledTableCell align="center">{content.title}</StyledTableCell>
-              {/* <StyledTableCell align="right">{content.title}</StyledTableCell> */}
-              <StyledTableCell align="center">
-                {content.category}
-              </StyledTableCell>
-              <StyledTableCell align="center">
-                <a href={content.url}>参考サイトへ</a>
-              </StyledTableCell>
-              <StyledTableCell align="center">
-                {cutDescription(content.description)}
-              </StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer component={Paper} className={classes.root}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center">タイトル</StyledTableCell>
+              <StyledTableCell align="center">カテゴリー</StyledTableCell>
+              <StyledTableCell align="center">URL</StyledTableCell>
+              <StyledTableCell align="center">説明</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {contents.slice(offset, offset + perPage).map((content) => (
+              <StyledTableRow>
+                <StyledTableCell align="center">
+                  {content.title}
+                </StyledTableCell>
+                {/* <StyledTableCell align="right">{content.title}</StyledTableCell> */}
+                <StyledTableCell align="center">
+                  {content.category}
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  <a href={content.url}>参考サイトへ</a>
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  {cutDescription(content.description)}
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <ReactPaginate
+        previousLabel={"<"}
+        nextLabel={">"}
+        breakLabel={"..."}
+        pageCount={Math.ceil(contents.length / perPage)} // 全部のページ数。端数の場合も考えて切り上げに。
+        marginPagesDisplayed={2} // 一番最初と最後を基準にして、そこからいくつページ数を表示するか
+        pageRangeDisplayed={5} // アクティブなページを基準にして、そこからいくつページ数を表示するか
+        onPageChange={handlePageChange} // クリック時のfunction
+        containerClassName={"pagination"} // ページネーションであるulに着くクラス名
+        // subContainerClassName={"pages pagination"}
+        activeClassName={"active"} // アクティブなページのliに着くクラス名
+        previousClassName={"pagination__previous"} // 「<」のliに着けるクラス名
+        nextClassName={"pagination__next"} // 「>」のliに着けるクラス名
+        disabledClassName={"pagination__disabled"} // 使用不可の「<,>」に着くクラス名
+      />
+    </>
   );
 };
 
